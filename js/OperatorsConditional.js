35f100f8535707a300cc580bbc882f9d573940d6
EshantBagwe <eshantbagwe2000@gmail.com>
2023-12-17 12:38:54 +0530

    practice question updated

--- js/OperatorsConditional.js
+++ js/OperatorsConditional.js
@@ -8,7 +8,6 @@
 // console.log(a%b);
 // console.log(a**b);
 
-
 // unary Operator
 //  a = a+1;
 // a++;
@@ -21,7 +20,7 @@
 //Assignment Operators
 // let a=5;
 // let b=2;
-// a+=4; 
+// a+=4;
 
 // comparision Operators
 // console.log("a == b",a!=b);
@@ -51,9 +50,9 @@
 
 // let num = 10;
 // if(num%2 === 0){
-    // console.log(num,"is even");
+// console.log(num,"is even");
 // }else{
-    // console.log(num,"is odd");
+// console.log(num,"is odd");
 // }
 
 // Ternary Operator
@@ -89,7 +88,6 @@
 // grade= "F"
 // }
 // console.log("according to entered marks",marks,"your grade is",grade);
-
 
 // practice question 1
 
@@ -132,3 +130,36 @@
 // console.log("b=",b);
 
 // practice question 4
+// let value = prompt("enter value")
+// let num = value%2;
+// switch (num){
+//   case 0:
+//     console.log(`${value} is a even no`);
+//     break;
+//   case 1:
+//     console.log(`${value} is a odd no`);
+//     break; 
+// }
+
+// practice question 5
+
+// let marks = prompt("Enter your marks:");
+// if(marks>=80){
+//     grade='A';
+//     console.log("your grade is",grade);
+// }else if(marks>=70 && marks<80){
+//     grade='B';
+//     console.log("your grade is",grade);
+// }else if(marks>=60 && marks<70){
+//     grade='C';
+//     console.log("your grade is",grade);
+// }else if(marks>=50 && marks<60){
+//     grade='D';
+//     console.log("your grade is",grade);
+// }else if(marks>=35 && marks<50){
+//     grade='E';
+//     console.log("your grade is",grade);
+// }else if(marks<35){
+//     grade='F';
+//     console.log("your grade is",grade);
+// }
